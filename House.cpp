77d17a9a42fcaf609806b24a94fc1ebe8b090384
John29 <john290883@mail.ru>
2022-04-07 18:53:56 +0300

    lesson_4

---
+++ House.cpp
@@ -0,0 +1,57 @@
+
+#include <iostream>
+
+#include "House.h"
+#include "MyTools.h"
+
+using namespace std;
+using namespace MyTools;
+
+bool House::isInside(double x1, double x2) const
+{
+	const double XBeg = x + 2;
+	const double XEnd = x + width - 1;
+
+	if (x1 < XBeg && x2 > XEnd)
+	{
+		return true;
+	}
+
+	if (x1 > XBeg && x1 < XEnd)
+	{
+		return true;
+	}
+
+	if (x2 > XBeg && x2 < XEnd)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+void House::Draw() const
+{
+	/*MyTools::SetColor(CC_Yellow);
+	GotoXY(x, y - 5);
+	cout << "  ########  ";
+	GotoXY(x, y - 4);
+	cout << "##        ##";
+	GotoXY(x, y - 3);
+	cout << "############";
+	GotoXY(x, y - 2);
+	cout << "#          #";
+	GotoXY(x, y - 1);
+	cout << "#          #";
+	GotoXY(x, y);
+	cout << "############";*/
+
+	for (int i = 0; i < Y; ++i) 
+	{
+		GotoXY(x, y - (Y - i - 1));
+		for (int j = 0; j < X; ++j)
+		{
+			cout << view[i][j];
+		}
+	}
+}
